2ed1ce0bf431d4440a6d39bd7c41fe64160293e2
Barkir <r.barinov2006@gmail.com>
2024-08-13 02:30:18 +0400

    added equation_solver function

--- main.cpp
+++ main.cpp
@@ -4,11 +4,12 @@
 //THIS PROGRAM CAN SOLVE THE SQUARE EQUATION
 
 void get_number(double * num);
+void equation_solver(double a, double b, double c);
 
 int main(void){
 
     double a = 0, b = 0, c = 0;
-    double d = 0;
+
     double x1 = 0, x2 = 0;
     printf("|Square Equation Solver|\n");
     printf("Enter a:_____\b\b\b\b\b");
@@ -19,23 +20,7 @@ int main(void){
     get_number(&c);
 
     printf("Your equation is: %.1lfx^2%+.1lfx%+.1lf\n", a, b, c);
-
-    d = pow(b, 2) - 4 * a * c;
-    if (d > 0)
-    {
-        x1 = ((-b + sqrt(d)) / (2 * a)); x2 = ((-b - sqrt(d)) / (2 * a));
-        printf("x1 = %.1lf\nx2 = %.1lf", x1, x2);
-    }
-    else if (d == 0.0)
-    {
-        x1 = (-b / (2 * a));
-        printf("x = %.1lf", x1);
-    }
-
-    else if (d < 0)
-    {
-        printf("No solutions.");
-    }
+    equation_solver(a, b, c);
 
     return 0;
 }
@@ -52,6 +37,30 @@ void get_number(double * num)
         printf("NOT A NUMBER!\n");
     }
 
+
+}
+
+
+void equation_solver(double a, double b, double c)
+{
+    double d = 0;
+    double x1 = 0, x2 = 0;
+    d = pow(b, 2) - 4 * a * c;
+    if (d > 0)
+    {
+        x1 = ((-b + sqrt(d)) / (2 * a)); x2 = ((-b - sqrt(d)) / (2 * a));
+        printf("x1 = %.1lf\nx2 = %.1lf", x1, x2);
+    }
+    else if (d == 0)
+    {
+        x1 = (-b / (2 * a));
+        printf("x = %.1lf", x1);
+    }
+
+    else if (d < 0)
+    {
+        printf("No solutions.");
+    }
 
 }
 
