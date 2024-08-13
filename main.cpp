ecad93fee645ac0d938d6bc11239624b8f9358a3
Barkir <r.barinov2006@gmail.com>
2024-08-13 14:03:37 +0400

    edited function equation_solver

--- main.cpp
+++ main.cpp
@@ -2,17 +2,15 @@
 #include <ctype.h>
 #include <math.h>
 
-#define TWO 2
-#define ONE 1
+#define SIZE 2
 //THIS PROGRAM CAN SOLVE THE SQUARE EQUATION
 
 void get_number(double * num);
-double * equation_solver(double a, double b, double c);
+void equation_solver(double a, double b, double c, double * x1, double * x2);
 
 int main(void){
 
     double a = 0, b = 0, c = 0;
-    double * array;
 
     double x1 = 0, x2 = 0;
     printf("|Square Equation Solver|\n");
@@ -24,10 +22,15 @@ int main(void){
     get_number(&c);
 
     printf("Your equation is: %.1lfx^2%+.1lfx%+.1lf\n", a, b, c);
-    array = equation_solver(a, b, c);
-    for (int i = 0; array[i] != '\0'; i++){
-        printf("x = %.1lf\n", array[i]);
+    equation_solver(a, b, c, &x1, &x2);
+    double array[SIZE] = {x1, x2};
+
+    for (int i = 0; i < 2; i++){
+        if (array[i] != NULL){printf("x = %.1lf\n", array[i]);}
+
+
     }
+
 
     return 0;
 }
@@ -48,32 +51,24 @@ void get_number(double * num)
 }
 
 
-double * equation_solver(double a, double b, double c)
+void equation_solver(double a, double b, double c, double * x1, double * x2)
 {
     double d = 0;
-    double x1 = 0, x2 = 0;
 
     d = pow(b, 2) - 4 * a * c;
     if (d > 0)
     {
-        static double array[TWO];
-        x1 = ((-b + sqrt(d)) / (2 * a)); x2 = ((-b - sqrt(d)) / (2 * a));
-        array[0] = x1; array[1] = x2;
-        return array;
+        *x1 = ((-b + sqrt(d)) / (2 * a)); *x2 = ((-b - sqrt(d)) / (2 * a));
     }
     else if (d == 0)
     {
-        static double array[ONE];
-        x1 = (-b / (2 * a));
-        array[0] = x1;
-        return array;
+        *x1 = (-b / (2 * a));
+        *x2 = NULL;
     }
 
     else if (d < 0)
     {
-        static double array[ONE];
-        array[0] = NULL;
-        return array;
+        *x1 = NULL; *x2 = NULL;
 
    }
 }
