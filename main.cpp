8b34dfe8ff8ddcd159e3c4ea7c95b84ffeab1039
Barkir <r.barinov2006@gmail.com>
2024-08-17 00:13:51 +0400

    get_number returns exit_failure

--- main.cpp
+++ main.cpp
@@ -3,22 +3,23 @@
 
 //THIS PROGRAM CAN SOLVE THE SQUARE EQUATION
 
-void get_number(double * num);
+int get_number(double * num);
 int equation_solver(double a, double b, double c, double * x1, double * x2);
 
 int main(void){
 
     double a = 0, b = 0, c = 0;
     int cnt = 0;
+    char check;
 
     double x1 = 0, x2 = 0;
     printf("|Square Equation Solver|\n");
     printf("Enter a:_____\b\b\b\b\b");
-    get_number(&a);
+    if (check = get_number(&a) == EXIT_FAILURE){exit(1);}
     printf("Enter b:_____\b\b\b\b\b");
-    get_number(&b);
+    if (check = get_number(&b) == EXIT_FAILURE){exit(1);}
     printf("Enter c:_____\b\b\b\b\b");
-    get_number(&c);
+    if (check = get_number(&c) == EXIT_FAILURE){exit(1);}
 
     printf("Your equation is: %.1lfx^2%+.1lfx%+.1lf\n", a, b, c);
     cnt = equation_solver(a, b, c, &x1, &x2);
@@ -47,15 +48,16 @@ int main(void){
 
 
 
-void get_number(double * num)
+int get_number(double * num)
 {
     char empty = -1, eof_check = -1;
     while ((eof_check = scanf("%lf", num)) != 1)
     {
-        if (eof_check == EOF){exit(1);}
+        if (eof_check == EOF){return EXIT_FAILURE;}
         while ((empty = getchar()) != '\n'){}
         printf("NOT A NUMBER!\n");
     }
+    return 0;
 }
 
 
