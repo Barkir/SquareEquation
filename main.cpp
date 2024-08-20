52b9e0debcc9e107d8982b2b11420872055f002c
Barkir <r.barinov2006@gmail.com>
2024-08-20 16:07:01 +0400

    created new fucntions
    int is_null(double num);
    double discriminant(double a, double b, double c);
    void root(int root_number, double x1, double x2);
    added assert;

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <math.h>
 #include <stdlib.h>
 #include <cstdlib>
+#include <assert.h>
 //THIS PROGRAM CAN SOLVE THE SQUARE EQUATION
 
 const int INF_SOLUTIONS = -1;
@@ -9,11 +10,14 @@ const double eps = 10e-8;
 int get_number(double * num);
 int solve_square_equation(double a, double b, double c, double * x1, double * x2);
 int solve_linear_equation(double a, double b, double * x1);
+int is_null(double num);
+double discriminant(double a, double b, double c);
+void root(int root_number, double x1, double x2);
 
 int main(void){
 
     double a = 0, b = 0, c = 0;
-    int cnt = 0;
+    int root_number = 0;
     char check;
 
     double x1 = 0, x2 = 0;
@@ -26,31 +30,9 @@ int main(void){
     if (check = get_number(&c) == EXIT_FAILURE){exit(EXIT_FAILURE);}
 
     printf("Your equation is: %.1lfx^2%+.1lfx%+.1lf\n", a, b, c);
-    cnt = solve_square_equation(a, b, c, &x1, &x2);
+    root_number = solve_square_equation(a, b, c, &x1, &x2);
 
-    switch(cnt)
-    {
-        case 0:
-        printf("No solutions.");
-        break;
-
-        case 1:
-        printf("x = %lf", x1);
-        break;
-
-        case 2:
-        printf("x1 = %lf\nx2 = %lf", x1, x2);
-        break;
-
-        case INF_SOLUTIONS:
-        printf("Infinite number of solutions.");
-        break;
-
-        default:
-        printf("Abort on line %d in file %s", __LINE__, __FILE__);
-        abort();
-        break;
-    }
+    root(root_number, x1, x2);
 
     return 0;
 }
@@ -59,6 +41,7 @@ int main(void){
 
 int get_number(double * num)
 {
+    assert(num != NULL);
     char empty = -1, eof_check = -1;
     while ((eof_check = scanf("%lf", num)) != 1)
     {
@@ -82,12 +65,15 @@ int get_number(double * num)
 int solve_square_equation(double a, double b, double c, double * x1, double * x2)
 {
 
-    if (abs(a) <= eps)
+    assert(x1 != NULL);
+    assert(x2 != NULL);
+
+    if (is_null(a))
     {
         return solve_linear_equation(b, c, x1);
     }
 
-    double d = (b*b) - 4 * a * c;
+    double d = discriminant(a, b, c);
     if (d > 0)
     {
         double sqrt_d = sqrt(d);
@@ -95,28 +81,66 @@ int solve_square_equation(double a, double b, double c, double * x1, double * x2
         *x2 = ((-b - sqrt_d) / (2 * a));
         return 2;
     }
-
-    else if (abs(d) < eps)
+    else if (is_null(d))
     {
         *x1 = (-b / (2 * a));
         return 1;
     }
-
-    else {return 0;}
     return 0;
 }
 
 
 int solve_linear_equation(double a, double b, double * x)
 {
-    if (abs(a) < eps)
+
+    assert(x != NULL);
+    if (is_null(a))
     {
-        if (abs(b) < eps) {return INF_SOLUTIONS;}
-        else {return 0;}
+        if (is_null(b)) {return INF_SOLUTIONS;}
+        return 0;
     }
-    else
+
+    *x = -b/a;
+    return 1;
+}
+
+
+int is_null(double a)
+{
+    return fabs(a) < eps;
+}
+
+double discriminant(double a, double b, double c)
+{
+    return b * b - 4 * a * c;
+}
+
+
+void root(int root_number, double x1, double x2)
+{
+    assert(isfinite(x1));
+    assert(isfinite(x2));
+
+     switch(root_number)
     {
-        *x = -b/a;
-        return 1;
+        case 0:
+        printf("No solutions.");
+        break;
+
+        case 1:
+        printf("x = %lf", x1);
+        break;
+
+        case 2:
+        printf("x1 = %lf\nx2 = %lf", x1, x2);
+        break;
+
+        case INF_SOLUTIONS:
+        printf("Infinite number of solutions.");
+        break;
+
+        default:
+        assert(0);
+        break;
     }
 }
